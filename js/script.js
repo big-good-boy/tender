1c1df3a849906ea2c89fe04d10965cbd17c7110c
big-good-boy <good_boy@live.ru>
2024-09-24 20:12:38 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-
-  
-
-  
- 
+const templateMenu = document.querySelector('.menu');
+if (templateMenu) {
+	if (document.querySelector('.menu__icon')) {
+		document.querySelectorAll('.menu__icon').forEach((el) => {
+			el.addEventListener('click', () => {
+				document.querySelector('body').classList.toggle('lock');
+				templateMenu.classList.toggle('menu--open');
+			});
+		});
+	}
+	if (templateMenu.querySelector('a')) {
+		templateMenu.querySelectorAll('a').forEach((el) => {
+			el.addEventListener('click', () => {
+				document.querySelector('body').classList.remove('lock');
+				templateMenu.classList.remove('menu--open');
+			});
+		});
+	}
+}
